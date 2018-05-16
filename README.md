9e02585f6f9642f5d9bcfb6641e5a1dc2eedbe08
dok <dumansky@gmail.com>
2018-05-16 18:09:59 +0300

    event api

--- README.md
+++ README.md
@@ -1,11 +1,7 @@
-# 🌮 Taco extension boilerplate 🌮
+# 🌮 Taco Event API 🌮
 ![MIT License](https://img.shields.io/github/license/TwiztedDesign/taco.svg)
 
 
-## Usage
-`git clone https://github.com/TwiztedDesign/taco-extension-boilerplate.git [your-extension-name]`
-
-And then change the EXTENSION_NAME variable in the index.html file
-
 ## 🍻 Cheers 🍻 
+
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 if(!window.taco) throw new Error('Taco is not defined');
-var taco = window.taco;
+let taco = window.taco;
 
-var EXTENSION_NAME = 'taco-extension-name';
+const EXTENSION_NAME = 'eventApi',
+      EVENT          = 'taco-event-query';
+
 
 taco[EXTENSION_NAME] = {
-    //All the extension functions go here
+
+    all : (cb) => {
+        query('', cb);
+    }
 };
+
+function query(q, cb){
+    taco.request(EVENT, {
+        query : q
+    }, function(res){
+        cb(res);
+    });
+}
 
